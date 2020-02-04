686690ef7de2adbab460cdef468b97712c9d26a3
Matheus Ferreira Neves <dev@perugluglu.net>
2020-02-04 18:27:15 -0300

    atualizado

--- forca.js
+++ forca.js
@@ -1,105 +1,100 @@
-var palavrachave = ["cachorro", "corvo", "camelo", "alemanha", "frança", "belgica", "mouse", "teclado", "caneta", "igor", "leo", "vitoria", "nike", "adidas", "mizuno"];
-var qtd = palavrachave.length - 1;
-var pos = Math.round(Math.random() * qtd);
-var palavra = palavrachave[pos];
-var cxletras = [];
-var acertos
-var errosmax = 7;
-var erros = 0;
-var desenho = [];
-var acertou = false;
-var jogando = false;
+var biblioteca=["cachorro", "corvo", "camelo", "alemanha", "frança", "belgica", "mouse", "teclado", "caneta", "igor", "leo", "vitoria", "nike", "adidas", "mizuno"];
+var qtde=biblioteca.length-1;
+var pos=Math.round(Math.random()*qtde);
+var palavra=biblioteca[pos];
+var tam=palavra.length;
+var cxletras=[];
+var acertos;
+var errosMax=7;
+var erros=0;
+var desenhos=[];
+var acertou=false;
+var jogando=false;
 var jog;
+
+function inicia() {
+    jogando=true;
+    jog=document.getElementById("letraj");
+    jog.value="";
+    jog.focus();
+    acertos=0;
+    erros=0;
+    acertou=false;
+    document.getElementById("forca").innerHTML="Letras Digitadas:";
+    pos=Math.round(Math.random()*qtde);
+    palavra=biblioteca[pos];
+    tam=palavra.length;
+    defineletras(tam);
+    document.getElementById("dvmsg").innerHTML="";
+    desenhos[1]=document.getElementById("cabeça");
+    desenhos[2]=document.getElementById("corpo");
+    desenhos[3]=document.getElementById("braçoD");
+    desenhos[4]=document.getElementById("braçoE");
+    desenhos[5]=document.getElementById("pernaE");
+    desenhos[6]=document.getElementById("pernaD");
+    document.getElementById("cabeça").src="img/cabeçaperdeu.jpg";
+    for(var i=1;i<7;i++){
+        desenhos[i].style.display="none";
+    }
+}
+
 
 function defineletras(l) {
     var obj;
-    for (var i = 0; i < 20; i++) {
-        obj = document.getElementById("letra" + i).nodeValue = "";
-        obj = document.getElementById("letra" + i).style.display = "none";
+    for(var i=0;i<20;i++) {
+        obj=document.getElementById("letra"+i).Value="";
+        obj=document.getElementById("letra"+i).style.display="none";
     }
-    for (var i = 0; i < l; i++) {
-        obj = document.getElementById("letra" + i).style.display = "inline-block";
+    for (var i=0;i<l;i++) {
+        obj=document.getElementById("letra"+i).style.display="inline-block";
     }
 }
 
 function jogar() {
-    //  jog = document.getElementById("letrajog");
-    jog = focus();
-    if (jog.Value == "") {
+    jog.focus();
+    if (jog.Value=="") {
         alert("digite uma letra");
-    } else {
+    }else{
         if (jogando) {
-
             var obj;
             var letraTmp;
             var letra;
             var pesq;
-
-            letra = jog.value;
-            jog.value = "";
-            acertou = false;
-            pesq = palavra.match(letra);
-
-            while (pesq != null) {
-                letraTmp = palavra.search(letra);
-                obj = document.getElementsByClassName("letra" + letraTmp).value = letra;
-                palavra = palavra.replace(letra, '0');
+            letra=jog.value;
+            jog.value="";
+            acertou=false;
+            pesq=palavra.match(letra);
+            while(pesq!=null){
+                letraTmp=palavra.search(letra);
+                obj=document.getElementById("letra"+letraTmp).value=letra;
+                palavra=palavra.replace(letra,'0');
                 acertos++;
-                pesq = palavra.match(letra);
-                acertou = true;
+                pesq=palavra.match(letra);
+                acertou=true;
             }
-            if (!acertou) {
-                document.getElementById("letrasdigi").innerHTML += letra.toUpperCase();
+            if(!acertou){
+                document.getElementById("forca").innerHTML+=letra.toUpperCase() + " ";
                 erros++;
-                if (erros < 7) {
-                    desenho[erro].style.display = "block";
-                }
-                else {
-                    document.getElementById("cabeçaPER").src = "img/cabeçaperdeu.jpg";
-                    document.getElementById("msg").innerHTML = "PERDEU";
-                    jogando = false;
+                if (erros<7) {
+                    desenhos[erros].style.display="block";
+                }else{
+                    document.getElementById("forca").src="img/cabeçaperdeu.jpg";
+                    document.getElementById("dvmsg").innerHTML="PERDEU";
+                    jogando=false;
                 }
             }
-            if (acertos == tam) {
-                document.getElementById("msg").innerHTML = "";
-                document.getElementById("msg").innerHTML = "GANHOU";
-                jogando = false;
+            if (acertos==tam) {
+                
+                document.getElementById("dvmsg").innerHTML="GANHOU";
+                jogando=false;
             }
         }
     }
 }
-
-function inicia() {
-    jogando = true;
-    jog = document.getElementById("letradigi");
-    jog.value = "";
-    jog.focus();
-    acertos = 0;
-    erro = 0;
-    acertou = false;
-    document.getElementById("letrasdigi").innerHTML = "Letras Digitadas";
-    pos = Math.round(Math.random() * qtd);
-    palavra = palavrachave[pos];
-    tam = palavra.length;
-    defineletras(tam);
-    document.getElementById("msg").innerHTML = "";
-    desenho[1] = document.getElementById("cabeça");
-    desenho[2] = document.getElementById("corpo");
-    desenho[3] = document.getElementById("braçoD");
-    desenho[4] = document.getElementById("braçoE");
-    desenho[5] = document.getElementById("pernaE");
-    desenho[6] = document.getElementById("pernaD");
-    document.getElementById("cabeça").src = "img/cabeça.jpg";
-    for (var i = 1; i < 7; i++) {
-        desenho[i].style.display = "none";
-    }
-}
-
 function dica() {
     alert(palavra);
     jog.focus();
 }
-
 window.addEventListener("load", inicia);
 
 
